8edc3201620fe23f571cd39cf157e520744c7c77
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-10-28 16:44:30 +0000

    deploy from master

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.4.0'
 set :application, 'parcc'
 set :repo_url, 'git@github.com:unepwcmc/parcc.git'
 
-set :branch, 'LinodeCap3Deploy'
+set :branch, 'master'
 
 set :deploy_user, 'wcmc'
 set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"

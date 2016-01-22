78b1100500b3bae3b1688b7c5b673bd853790133
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-01-22 12:28:42 +0000

    handle case in which sections have no children

--- app/helpers/cms_helper.rb
+++ app/helpers/cms_helper.rb
@@ -4,11 +4,15 @@ module CmsHelper
   def cms_section section, current_page
     children = section.children
 
-    concat content_tag(:h3, section.label, class: cms_section_classes(children, current_page))
-    content_tag(:div, {class: "#{'u-hide' if !children.include?(current_page)}"}) do
-      children.map { |page|
-        cms_section_page(current_page, page)
-      }.join("\n").html_safe
+    if children.any?
+      concat content_tag(:h3, section.label, class: cms_section_classes(children, current_page))
+      content_tag(:div, {class: "#{'u-hide' if !children.include?(current_page)}"}) do
+        children.map { |page|
+          cms_section_page(current_page, page)
+        }.join("\n").html_safe
+      end
+    else
+      cms_section_page(current_page, section)
     end
   end
 
